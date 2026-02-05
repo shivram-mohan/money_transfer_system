98cebf617875734dfd84a5afc09b8f2619e24eba
Shivram <ramshiv590@gmail.com>
2026-02-05 07:16:52 +0000

    module 2 added

---
+++ backend/BackendProj/src/main/java/com/fidelity/moneytransfer/dto/ErrorResponse.java
@@ -0,0 +1,20 @@
+package com.fidelity.moneytransfer.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ErrorResponse {
+    private String errorCode;
+    private String message;
+    private LocalDateTime timestamp;
+    private String path;
+
+}
